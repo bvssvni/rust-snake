90d197d7d7fd05f1f91826479a8de3396859b525
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 16:10:32 +0200

    Using `clear` from Rust-Graphics

--- src/snakeapp.rs
+++ src/snakeapp.rs
@@ -2,7 +2,8 @@
 use Settings = piston::game::Settings;
 use Game = piston::game::Game;
 use GameWindow = piston::game_window::GameWindow;
-use Gl = piston::gl::Gl;
+use piston::gl::Gl;
+use graphics;
 use graphics::*;
 use rand::random;
 
@@ -23,7 +24,6 @@ fn load_colors() -> Vec<f32> {
 }
 
 pub struct SnakeApp {
-    gl: Option<Gl>,
     vertices: Vec<f32>,
     colors: Vec<f32>,
     settings: Settings,
@@ -33,25 +33,22 @@ pub struct SnakeApp {
 impl Game for SnakeApp {
     fn get_game_window<'a>(&'a self) -> &'a GameWindow { &self.game_window }
     fn get_settings<'a>(&'a self) -> &'a Settings { &self.settings }
-    fn render(&self) {
+    fn render(&self, c: &graphics::Context, gl: &mut Gl) {
         // Render triangle.
         // self.shader.unwrap().render(self.vertices.as_slice(), self.colors.as_slice());
 
-        let mut gl = self.gl.unwrap();
-        let c = Context::new();
         for _ in range(0, 1 << 10) {
             let x: f64 = random();
             let y: f64 = random();
             c
             .rect(x - 0.5, y - 0.5, 0.005, 0.005)
             .rgba(random(), 0.0, 0.0, 1.0)
-            .fill(&mut gl);
+            .fill(gl);
         }
     }
     fn update(&mut self) {
     }
     fn load(&mut self) {
-        self.gl = Some(Gl::new());
     }
 }
 
@@ -60,7 +57,6 @@ impl SnakeApp {
         let exit_on_esc = true;
         let background_color = [1.0, 1.0, 1.0, 1.0];
         SnakeApp {
-            gl: None,
             game_window: GameWindow::window("Snake", 512, 512),
             vertices: load_vertices(),
             colors: load_colors(),
