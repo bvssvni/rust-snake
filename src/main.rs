4dc34ae4e2cab43b08d6f10c93e2986a65d228db
Sven Nilsen <bvssvni@gmail.com>
2014-11-10 21:37:54 +0100

    Using current objects

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,20 @@ extern crate gfx_graphics;
 extern crate gfx;
 extern crate sdl2;
 
-use current::{ Get, Set };
+use shader_version::opengl;
+use current::{ Current, Get, Set, Usage, UseCurrent };
 use std::cell::RefCell;
 use opengl_graphics::Gl;
 use gfx_graphics::G2D;
 use gfx::{Device, DeviceHelper};
-use sdl2_window::Sdl2Window;
+use sdl2_window::Sdl2Window as Window;
 use event::{
     Events, WindowSettings,
     Render, Update, Input,
 };
 use event::window::{ Title };
 use fps_counter::FPSCounter;
+use snakeapp::SnakeApp;
 
 mod snakeapp;
 mod object;
@@ -46,14 +48,13 @@ pub enum GraphicsBackEnd {
 }
 
 fn main() {
-    use snakeapp::SnakeApp;
 
-    let mut backend = Gfx;
+    let backend = Gfx;
     println!("Running with graphics backend {}", backend);
     println!("Use 'S' to swap back-end");
 
     let opengl = shader_version::opengl::OpenGL_3_2;
-    let window = Sdl2Window::new(
+    let window = Window::new(
         opengl,
         WindowSettings {
             title: "Sea Snake Escape".to_string(),
@@ -64,32 +65,78 @@ fn main() {
         }
     );
 
-    let mut device = gfx::GlDevice::new(|s| unsafe {
+    let device = gfx::GlDevice::new(|s| unsafe {
         std::mem::transmute(sdl2::video::gl_get_proc_address(s))
     });
-    let event::window::Size([w, h]) = window.get();
+
+    let window = RefCell::new(window);
+    let backend = RefCell::new(backend);
+    let device = RefCell::new(device);
+    let opengl = RefCell::new(opengl);
+
+    let window_guard = window.set_current();
+    let backend_guard = backend.set_current();
+    let device_guard = device.set_current();
+    let opengl_guard = opengl.set_current();
+    start();
+    drop(opengl_guard);
+    drop(device_guard);
+    drop(backend_guard);
+    drop(window_guard);
+}
+
+fn current_window() -> Usage<'static, Window> { UseCurrent }
+
+fn current_gfx_device() -> Usage<'static, gfx::GlDevice> { UseCurrent }
+
+fn current_opengl() -> Usage<'static, opengl::OpenGL> { UseCurrent }
+
+fn current_graphics_back_end() -> Usage<'static, GraphicsBackEnd> { UseCurrent }
+
+fn swap_backend<E: event::GenericEvent>(e: &E) {
+    use event::{ PressEvent };
+    e.press(|button| {
+        if button == input::Keyboard(input::keyboard::S) {
+            *current_graphics_back_end() = match *current_graphics_back_end() {
+                    Gfx => { println!("Swapped to OpenGL"); OpenGL }
+                    OpenGL => { println!("Swapped to Gfx"); Gfx }
+                };
+        }
+    });
+}
+
+fn set_title(text: String) {
+    current_window().set_mut(Title(text));
+}
+
+fn events() -> event::Events<current::Usage<'static, Window>> {
+    Events::new(current_window())
+}
+
+fn start() {
+    use event::window::Size;
+    let Size([w, h]) = current_window().get();
     let frame = gfx::Frame::new(w as u16, h as u16);
 
-    let mut renderer = device.create_renderer();
+    let ref mut gl = Gl::new(*current_opengl());
+    let mut g2d = G2D::new(&mut *current_gfx_device());
+    let mut renderer = current_gfx_device().create_renderer();
 
+    let mut fps_counter = FPSCounter::new();
     let mut app = SnakeApp::new();
     app.load();
-
-    let ref mut gl = Gl::new(opengl);
-    let mut g2d = G2D::new(&mut device);
-    let mut fps_counter = FPSCounter::new();
-    let mut window = &RefCell::new(window);
-    for e in Events::new(window) {
+    for e in events() {
+        swap_backend(&e);
         match e {
             Render(args) => {
-                match backend {
+                match *current_graphics_back_end() {
                     Gfx => {
                         g2d.draw(&mut renderer, &frame, |c, g| {
                             use graphics::*;
                             c.color(settings::WATER_COLOR).draw(g);
                             app.render(&c, g);
                         });
-                        device.submit(renderer.as_buffer());
+                        current_gfx_device().submit(renderer.as_buffer());
                         renderer.reset();
                     }
                     OpenGL => {
@@ -105,17 +152,11 @@ fn main() {
                     }
                 };
 
-                window.set_mut(Title(fps_counter.tick().to_string()));
+                set_title(fps_counter.tick().to_string());
             },
             Update(args) => {
                 app.update(args.dt);
             },
-            Input(input::Press(input::Keyboard(input::keyboard::S))) => {
-                backend = match backend {
-                        Gfx => { println!("Swapped to OpenGL"); OpenGL }
-                        OpenGL => { println!("Swapped to Gfx"); Gfx }
-                    };
-            }
             Input(input::Press(input::Keyboard(key))) => {
                 app.key_press(key);
             },
